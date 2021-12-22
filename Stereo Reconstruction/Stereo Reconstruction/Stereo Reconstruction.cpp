041a6104de93b6a9d7e53acb75c944f63edcfdf5
MomenAmgad <36383458+MomenAmgad@users.noreply.github.com>
2021-12-22 16:25:50 +0200

    Add files via upload
    Features detection and matching

--- Stereo Reconstruction/Stereo Reconstruction/Stereo Reconstruction.cpp
+++ Stereo Reconstruction/Stereo Reconstruction/Stereo Reconstruction.cpp
@@ -6,36 +6,83 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include "opencv2/features2d.hpp"
+#include "opencv2/core.hpp"
+
 
 
 using namespace cv;
 
 int main()
 {
-    //std::cout << "Hello World!\n";
-    //cv::Mat image = cv::imread("../img/sherry-christian-8Myh76_3M2U-unsplash.jpg");
-    //imshow("Display window", image);
+    std::cout << "Hello World!\n";
+    cv::Mat img1 = cv::imread("../img/data/artroom1/im0C.png", IMREAD_GRAYSCALE);
+    cv::Mat img2 = cv::imread("../img/data/artroom1/im1C.png", IMREAD_GRAYSCALE);
+  /*  if (img1.empty() || img2.empty())
+    {
+        cout << "Could not open or find the image!\n" << endl;
+        parser.printMessage();
+        return -1;
+    }*/
+    //-- Step 1: Detect the keypoints using SURF Detector, compute the descriptors
+    int minHessian = 400;
+    Ptr<FeatureDetector> detector = ORB::create();
+    std::vector<KeyPoint> keypoints1, keypoints2;
+    Mat descriptors1, descriptors2;
+    detector->detectAndCompute(img1, noArray(), keypoints1, descriptors1);
+    detector->detectAndCompute(img2, noArray(), keypoints2, descriptors2);
+
+    cv::Mat output;
+    cv::drawKeypoints(img1, keypoints1, output);
+    descriptors1.convertTo(descriptors1, CV_32F);
+    descriptors2.convertTo(descriptors2, CV_32F);
+
+  /*  FlannBasedMatcher matcher;
+    std::vector<DMatch> matches;
+    matcher.match(descriptors1, descriptors2, matches);*/
+
+    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::FLANNBASED);
+    std::vector< std::vector<DMatch> > knn_matches;
+    matcher->knnMatch(descriptors1, descriptors2, knn_matches, 2);
+
+    //-- Filter matches using the Lowe's ratio test
+    const float ratio_thresh = 0.7f;
+    std::vector<DMatch> good_matches;
+    for (size_t i = 0; i < knn_matches.size(); i++)
+    {
+        if (knn_matches[i][0].distance < ratio_thresh * knn_matches[i][1].distance)
+        {
+            good_matches.push_back(knn_matches[i][0]);
+        }
+    }
+    //-- Draw matches
+    Mat img_matches;
+    drawMatches(img1, keypoints1, img2, keypoints2, good_matches, img_matches, Scalar::all(-1),
+        Scalar::all(-1), std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+    //-- Show detected matches
+    imshow("Good Matches", img_matches);
+
+    
+
+
+    int k = waitKey(0); // Wait for a keystroke in the window
+   
+    return 0;
+
+    //std::string image_path = samples::findFile("starry_night.jpg");
+    //Mat img = imread(image_path, IMREAD_COLOR);
+    //if (img.empty())
+    //{
+    //    std::cout << "Could not read the image: " << image_path << std::endl;
+    //    return 1;
+    //}
+    //imshow("Display window", img);
     //int k = waitKey(0); // Wait for a keystroke in the window
     //if (k == 's')
     //{
-    //    imwrite("starry_night.png", image);
+    //    imwrite("starry_night.png", img);
     //}
     //return 0;
-
-    std::string image_path = samples::findFile("starry_night.jpg");
-    Mat img = imread(image_path, IMREAD_COLOR);
-    if (img.empty())
-    {
-        std::cout << "Could not read the image: " << image_path << std::endl;
-        return 1;
-    }
-    imshow("Display window", img);
-    int k = waitKey(0); // Wait for a keystroke in the window
-    if (k == 's')
-    {
-        imwrite("starry_night.png", img);
-    }
-    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
